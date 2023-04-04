95fcf15069ec7ec05c73522caa397e081b4f045e
Rubayed Ahmed <rubayedahmed04@gmail.com>
2023-04-04 11:20:44 +0600

    m

--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -1,107 +1,36 @@
-import React, { useState } from "react";
+import React from 'react';
 
-const Navbar = () => {
-  const [navbar, setNavbar] = useState(false);
+function Navbar() {
   return (
-    <nav className="w-full bg-purple-500 shadow">
-    <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
-        <div>
-            <div className="flex items-center justify-between py-3 md:py-5 md:block">
-                <a href="javascript:void(0)">
-                    <h2 className="text-2xl font-bold text-white">LOGO</h2>
-                </a>
-                <div className="md:hidden">
-                    <button
-                        className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
-                        onClick={() => setNavbar(!navbar)}
-                    >
-                        {navbar ? (
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="w-6 h-6 text-white"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                    clipRule="evenodd"
-                                />
-                            </svg>
-                        ) : (
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="w-6 h-6 text-white"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                strokeWidth={2}
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M4 6h16M4 12h16M4 18h16"
-                                />
-                            </svg>
-                        )}
-                    </button>
-                </div>
-            </div>
+    <nav className="bg-yellow-400 py-4">
+      <div className="max-w-6xl mx-auto px-4">
+        <div className="flex justify-between items-center">
+          <div className="flex">
+            <a className="text-gray-900 text-2xl font-bold hover:text-gray-700" href="#">Your Brand</a>
+          </div>
+          <div className="hidden md:block">
+            <a className="px-4 py-2 text-gray-900 hover:bg-yellow-500 rounded" href="#">Home</a>
+            <a className="px-4 py-2 text-gray-900 hover:bg-yellow-500 rounded" href="#">About</a>
+            <a className="px-4 py-2 text-gray-900 hover:bg-yellow-500 rounded" href="#">Services</a>
+            <a className="px-4 py-2 text-gray-900 hover:bg-yellow-500 rounded" href="#">Contact</a>
+          </div>
+          <div className="md:hidden">
+            <button type="button" className="text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900" aria-label="toggle menu">
+              <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
+                <path fillRule="evenodd" d="M3 6h18v2H3V6zm0 5h18v2H3v-2zm0 5h18v2H3v-2z"/>
+              </svg>
+            </button>
+          </div>    
         </div>
-        <div>
-            <div
-                className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-                    navbar ? "block" : "hidden"
-                }`}
-            >
-                <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-                    <li className="text-white hover:text-indigo-200">
-                        <a href="javascript:void(0)">Home</a>
-                    </li>
-                    <li className="text-white hover:text-indigo-200">
-                        <a href="javascript:void(0)">Blog</a>
-                    </li>
-                    <li className="text-white hover:text-indigo-200">
-                        <a href="javascript:void(0)">About US</a>
-                    </li>
-                    <li className="text-white hover:text-indigo-200">
-                        <a href="javascript:void(0)">Contact US</a>
-                    </li>
-                </ul>
-
-                <div className="mt-3 space-y-2 lg:hidden md:inline-block">
-            <a
-                href="javascript:void(0)"
-                className="inline-block w-full px-4 py-2 text-center text-white bg-gray-600 rounded-md shadow hover:bg-gray-800"
-            >
-                Sign in
-            </a>
-            <a
-                href="javascript:void(0)"
-                className="inline-block w-full px-4 py-2 text-center text-gray-800 bg-white rounded-md shadow hover:bg-gray-100"
-            >
-                Sign up
-            </a>
+        <div className="md:hidden pt-2">
+          <a className="block px-4 py-2 text-gray-900 hover:bg-yellow-500 rounded" href="#">Home</a>
+          <a className="block px-4 py-2 text-gray-900 hover:bg-yellow-500 rounded" href="#">About</a>
+          <a className="block px-4 py-2 text-gray-900 hover:bg-yellow-500 rounded" href="#">Services</a>
+          <a className="block px-4 py-2 text-gray-900 hover:bg-yellow-500 rounded" href="#">Contact</a>
         </div>
-            </div>
-        </div>
-        <div className="hidden space-x-2 md:inline-block">
-            <a
-                href="javascript:void(0)"
-                className="px-4 py-2 text-white bg-gray-600 rounded-md shadow hover:bg-gray-800"
-            >
-                Sign in
-            </a>
-            <a
-                href="javascript:void(0)"
-                className="px-4 py-2 text-gray-800 bg-white rounded-md shadow hover:bg-gray-100"
-            >
-                Sign up
-            </a>
-        </div>
-    </div>
-</nav>
+      </div>
+    </nav>
   );
-};
+}
 
 export default Navbar;
