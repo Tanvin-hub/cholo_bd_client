76425201233b1f25eb600721ea64c468cc764815
Maruf Hossain <marufrony48@gmail.com>
2023-04-06 01:43:28 +0600

    Contact Added

--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../components/Home/Home";
 import About from "../components/About/About";
+import Contact from "../components/Contact/Contact";
 
 const router = createBrowserRouter([
   {
@@ -16,7 +17,13 @@ const router = createBrowserRouter([
         path: "/about",
         element: <About />,
       },
+      {
+        path: "/contact",
+        element: <Contact />,
+      }
     ],
+
+
   },
 ]);
 
