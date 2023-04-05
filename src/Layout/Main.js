a580beb3b43fb5950f34615de2cbcc88503de7eb
Maruf Hossain <marufrony48@gmail.com>
2023-04-05 12:23:32 +0600

    first commit

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Navbar from '../components/Shared/Navbar/Navbar'
-import {Outlet} from 'react-router-dom'
+import {Outlet} from 'react-router-dom';
+import Navbar from '../components/Shared/Navbar/Navbar';
+import Home from "../components/Shared/Home/home"
+
 
 const Main = () => {
     return (
         <div>
             <Navbar/>
+            <Home/>
             <Outlet/>
         </div>
     );
