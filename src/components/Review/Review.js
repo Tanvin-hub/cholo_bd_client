aeefe8677a5a0da60a7649079d0b4d9d76bea95e
Maruf Hossain <marufrony48@gmail.com>
2023-04-09 03:17:05 +0600

    review

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -2,46 +2,55 @@ import React from 'react'
 
 const Review = () => {
   return (
-    <div>
-    <div class="min-h-screen  py-6 flex flex-col justify-center sm:py-12">
-<div class="py-3 sm:max-w-xl sm:mx-auto">
-<div class=" border border-primary min-w-1xl flex flex-col rounded-xl shadow-lg">
-  <div class="px-12 py-5">
-    <h2 class="text-gray-800 text-3xl font-semibold">Your opinion matters to us!</h2>
-  </div>
-  <div class="bg-gray-200 w-full flex flex-col items-center">
-    <div class="flex flex-col items-center py-6 space-y-3">
-      <span class="text-lg text-gray-800">How was quality of the call?</span>
-      <div class="flex space-x-3">
-        <svg class="w-12 h-12 text-yellow-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-        </svg>
-        <svg class="w-12 h-12 text-yellow-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-        </svg>
-        <svg class="w-12 h-12 text-yellow-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-        </svg>
-        <svg class="w-12 h-12 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-        </svg>
-        <svg class="w-12 h-12 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-        </svg>
-      </div>
-    </div>
-    <div class="w-3/4 flex flex-col">
-      <textarea rows="3" class="p-4 text-gray-500 rounded-xl resize-none">Leave a message, if you want</textarea>
-      <button class="py-3 my-8 text-lg bg-gradient-to-r from-purple-500 to-indigo-600 rounded-xl text-white">Rate now</button>
-    </div>
-  </div>
   
-</div>
-
-
-</div>
-</div>
-</div>
+    <section>
+      <div class="register bg-cover  text-white py-20">
+        <div class="container mx-auto flex flex-col md:flex-row my-6 md:my-24">
+          <div class="flex flex-col w-full lg:w-1/3 p-8">
+            <p class="ml-6 text-primary text-lg uppercase tracking-loose">REVIEW</p>
+            <p class="text-3xl md:text-5xl my-4 leading-relaxed md:leading-snug">Leave us a feedback!</p>
+            <p class="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
+              Please provide your valuable feedback and something something ...
+            </p>
+          </div>
+          <div class="flex flex-col w-full lg:w-2/3 justify-center">
+            <div class="container w-full px-4">
+              <div class="flex flex-wrap justify-center">
+                <div class="w-full lg:w-6/12 px-4">
+                  <div
+                    class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-white">
+                    <div class="flex-auto p-5 lg:p-10">
+                      <h4 class="text-2xl mb-4 text-black font-semibold">Have a suggestion?</h4>
+                      <form id="feedbackForm" action="" method="">
+                        <div class="relative w-full mb-3">
+                          <label class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                            for="email">Name</label><input type="email" name="email" id="email" class="border-0 px-3 py-3 rounded text-sm shadow w-full
+                        bg-gray-300 placeholder-black text-gray-800 outline-none focus:bg-gray-400" placeholder=" "
+                            style={{transition: 'all 0.15s ease 0s'}} required />
+                        </div>
+                          <div class="relative w-full mb-3">
+                            <label class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                            for="message">Message</label><textarea maxlength="300" name="feedback" id="feedback" rows="4"
+                            cols="80"
+                            class="border-0 px-3 py-3 bg-gray-300 placeholder-black text-gray-800 rounded text-sm shadow focus:outline-none w-full"
+                            placeholder="" required></textarea>
+                          </div>
+                          <div class="text-center mt-6">
+                            <button id="feedbackBtn"
+                            class="bg-yellow-300 text-black text-center mx-auto active:bg-yellow-400 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+                            type="submit" style={{transition: 'all 0.15s ease 0s'}}>Submit
+                          </button>
+                          </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
