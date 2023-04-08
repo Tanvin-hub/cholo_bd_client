2ef82b5c62d7a7d9eb91339fa40986b973fa3661
Rubayed Ahmed <rubayedahmed04@gmail.com>
2023-04-08 15:32:47 +0600

    Navbar bg color change

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -6,14 +6,29 @@ import "swiper/css/pagination";
 
 const Testimonial = () => {
   return (
-    <section className="my-20 lg:py-[120px]">
+    <section className="my-20">
+        <div className="flex flex-wrap -mx-4">
+          <div className="w-full px-4">
+            <div className="text-center mx-auto mb-12 lg:mb-20 max-w-[510px]">
+              <span className="font-semibold text-lg text-primary mb-2 block">
+                Testimonial
+              </span>
+              <h2 className="font-bold text-3xl sm:text-4xl md:text-[40px] text-dark mb-4">
+                What Client says about us
+              </h2>
+              <p className="text-base text-body-color">
+                There are many variations of passages of Lorem Ipsum available
+                but the majority have suffered alteration in some form.
+              </p>
+            </div>
+          </div>
+        </div>
       <Swiper
         pagination={{
           dynamicBullets: true,
         }}
         modules={[Pagination]}
-        className="mySwiper py-12"
-      >
+        className="mySwiper py-12">
         <SwiperSlide>Slide 1</SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
