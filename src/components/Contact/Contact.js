9ca7913fc6b0dfaab9c98d8427689fae78ea7d9c
Maruf Hossain <marufrony48@gmail.com>
2023-04-12 02:38:53 +0600

    update

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,14 +1,11 @@
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
-import { BsFillTelephoneFill,BsFillClockFill } from "react-icons/bs";
-import {MdLocationOn, MdOutlineMail} from "react-icons/md";
+import { BsFillTelephoneFill, BsFillClockFill } from "react-icons/bs";
+import { MdLocationOn, MdOutlineMail } from "react-icons/md";
 import GoogleMapReact from "google-map-react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 const Contact = () => {
-
   const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-
 
   const defaultProps = {
     center: {
@@ -17,7 +14,6 @@ const Contact = () => {
     },
     zoom: 11,
   };
-
 
   const form = useRef();
 
@@ -41,159 +37,202 @@ const Contact = () => {
       );
   };
 
-
-
   return (
     <div>
-    <section className="con-img text-white h-[500px] relative mb-[1300px]">
-      <div className="overlay h-[500px]">
-        <div className="py-12 md:py-20">
-          <div
-            className="text-white md:pt-42 md:absolute md:top-[35%] 
+      <section className="con-img text-white h-[500px] relative mb-[1300px]">
+        <div className="overlay h-[500px]">
+          <div className="py-12 md:py-20">
+            <div
+              className="text-white md:pt-42 md:absolute md:top-[35%] 
           md:left-1/2 md:-translate-x-2/4 md:-translate-y-2/4 flex flex-col items-center"
-          >
-            <h1
-              className="text-4xl md:text-5xl font-extrabold capitalize
-            sm:text-center heading"
             >
-             Contact Us
-            </h1>
-            
-            <ol class="list-reset flex">
-    <li>
-      <Link className='text-primary'>Home</Link>
-    </li>
-    <li>
-      <span class="mx-3 text-white">  / </span>
-    </li>
-    <li>
-      <Link className='text-primary'> Contact </Link>
-    
-    </li>
-    
-    
-  </ol>
-            
-          </div>
-        </div>
-      </div>
-
-      <div className="absolute border  -bottom-[100%] left-[5%] w-[90%] h-[110vh] mx-auto py-8 pl-6 pr-12 
-      rounded-2xl bg-white text-black shadow-xl">
-        <form className="grid grid-cols-4 gap-6 items-center">
-          <div className='shadow-2xl border py-4 ml-8 rounded '>
-            <div className='text-primary text-3xl ml-[104px] mb-4'>
-          <BsFillTelephoneFill />
-          </div>
-            <label className="font-medium text-xl ml-[88px] text-gray-600 ">Phone</label>
-               <h1 className='ml-8 mt-1 font-semibold text-xl text-gray-600'>+8801779580031</h1>
-          </div>
-
-          <div className='shadow-2xl border py-4 ml-8 rounded '>
-            <div className='text-primary text-4xl ml-[104px] mb-4'>
-          <MdLocationOn />
-          </div>
-            <label className="font-medium text-xl ml-20 text-gray-600 ">Address</label>
-               <h1 className='ml-10 mt-1 font-semibold text-lg text-gray-600'>Zindabazar,Sylhet</h1>
-          </div>
-
-          <div className='shadow-2xl border py-4 ml-8 rounded '>
-            <div className='text-primary text-4xl ml-24 mb-4'>
-          <MdOutlineMail />
-          </div>
-            <label className="font-medium text-xl ml-20 text-gray-600 ">E-Mail</label>
-               <h1 className='ml-[10px] mt-1 font-semibold text-base text-gray-600'>marufrony48@gmail.com</h1>
-          </div>
-          <div className='shadow-2xl border py-4 ml-8 rounded '>
-            <div className='text-primary text-4xl ml-[96px] mb-4'>
-          <BsFillClockFill />
-          </div>
-            <label className="font-medium text-xl ml-12 text-gray-600">Opening Time</label>
-               <h1 className=' mt-1 font-semibold text-lg ml-6 text-gray-600'>10:00 am to 23:00 pm</h1>
-          </div>
-
-        </form>
-        <div className='mt-10 ml-7'>
-        <div className="px-3 py-5">
-            <div style={{
-                height: "65vh",
-                width: "100%",
-              
-              }}>
-              <GoogleMapReact
-                bootstrapURLKeys={{ key: "" }}
-                defaultCenter={defaultProps.center}
-                defaultZoom={defaultProps.zoom}
+              <h1
+                className="text-4xl md:text-5xl font-extrabold capitalize
+            sm:text-center heading"
               >
-                <AnyReactComponent
-                  lat={59.955413}
-                  lng={30.337844}
-                  text="My Marker"
-                />
-              </GoogleMapReact>
-            </div>
-   
+                Contact Us
+              </h1>
 
-
-
-          </div>
-          </div>
-
-
-          <div className='mt-48'>
-          <div className=" -mx-4">
-          <div className="w-full px-4">
-            <div className="text-center mx-auto mb-12 lg:mb-20 max-w-[510px]">
-              <span className="font-semibold text-lg text-primary mb-2 block">
-                Contact US
-              </span>
-              <h2 className="font-bold text-3xl sm:text-4xl md:text-[40px] text-dark ">
-              Leave Message
-              </h2>
-             
+              <ol class="list-reset flex">
+                <li>
+                  <Link className="text-primary">Home</Link>
+                </li>
+                <li>
+                  <span class="mx-3 text-white"> / </span>
+                </li>
+                <li>
+                  <Link className="text-primary"> Contact </Link>
+                </li>
+              </ol>
             </div>
           </div>
         </div>
 
-        <form ref={form} onSubmit={sendEmail} className="mx-auto mt-16  sm:mt-20">
-    <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-      <div>
-        <label for="first-name" className="block text-sm font-semibold leading-6 text-gray-900">First name</label>
-        <div className="mt-2.5">
-          <input type="text" name="first-name" id="first-name" autocomplete="given-name"  placeholder='Enter Your Name'className="block w-full  border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6"/>
-        </div>
-      </div>
+        <div
+          className="absolute border  -bottom-[100%] left-[5%] w-[90%] h-[110vh] mx-auto py-8 pl-6 pr-12 
+      rounded-2xl bg-white text-black shadow-xl"
+        >
+          <form className="grid grid-cols-4 gap-6 items-center">
+            <div className="shadow-2xl border py-4 ml-8 rounded ">
+              <div className="text-primary text-3xl ml-[104px] mb-4">
+                <BsFillTelephoneFill />
+              </div>
+              <label className="font-medium text-xl ml-[88px] text-gray-600 ">
+                Phone
+              </label>
+              <h1 className="ml-8 mt-1 font-semibold text-xl text-gray-600">
+                +8801779580031
+              </h1>
+            </div>
 
-      <div>
-        <label for="last-name" className="block text-sm font-semibold leading-6 text-gray-900">Email</label>
-        <div className="mt-2.5">
-        <input type="email" name="email" id="email" autocomplete="email" placeholder='Enter Your Email' className="block w-full  border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400   sm:text-sm sm:leading-6"/>
-        </div>
-      </div>
-      
-      <div className="sm:col-span-2">
-        <label for="message" className="block text-sm font-semibold leading-6 text-gray-900">Message</label>
-        <div className="mt-2.5">
-          <textarea name="message" id="message" rows="4" placeholder='Enter Your Message' className="block w-full  border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2  sm:text-sm sm:leading-6"></textarea>
-        </div>
-      </div>
-    
-    </div>
-    <div className="mt-10">
-      <button type="submit" className="ml-[500px] mb-32 rounded-md bg-primary px-10 py-2.5 text-center text-sm font-semibold text-white   ">Submit</button>
-    </div>
-  </form>
+            <div className="shadow-2xl border py-4 ml-8 rounded ">
+              <div className="text-primary text-4xl ml-[104px] mb-4">
+                <MdLocationOn />
+              </div>
+              <label className="font-medium text-xl ml-20 text-gray-600 ">
+                Address
+              </label>
+              <h1 className="ml-10 mt-1 font-semibold text-lg text-gray-600">
+                Zindabazar,Sylhet
+              </h1>
+            </div>
 
+            <div className="shadow-2xl border py-4 ml-8 rounded ">
+              <div className="text-primary text-4xl ml-24 mb-4">
+                <MdOutlineMail />
+              </div>
+              <label className="font-medium text-xl ml-20 text-gray-600 ">
+                E-Mail
+              </label>
+              <h1 className="ml-[10px] mt-1 font-semibold text-base text-gray-600">
+                marufrony48@gmail.com
+              </h1>
+            </div>
+            <div className="shadow-2xl border py-4 ml-8 rounded ">
+              <div className="text-primary text-4xl ml-[96px] mb-4">
+                <BsFillClockFill />
+              </div>
+              <label className="font-medium text-xl ml-12 text-gray-600">
+                Opening Time
+              </label>
+              <h1 className=" mt-1 font-semibold text-lg ml-6 text-gray-600">
+                10:00 am to 23:00 pm
+              </h1>
+            </div>
+          </form>
+          <div className="mt-10 ml-7">
+            <div className="px-3 py-5">
+              <div
+                style={{
+                  height: "65vh",
+                  width: "100%",
+                }}
+              >
+                <GoogleMapReact
+                  bootstrapURLKeys={{ key: "" }}
+                  defaultCenter={defaultProps.center}
+                  defaultZoom={defaultProps.zoom}
+                >
+                  <AnyReactComponent
+                    lat={59.955413}
+                    lng={30.337844}
+                    text="My Marker"
+                  />
+                </GoogleMapReact>
+              </div>
+            </div>
           </div>
-      </div>
-   
+
+          <div className="mt-48">
+            <div className=" -mx-4">
+              <div className="w-full px-4">
+                <div className="text-center mx-auto mb-12 lg:mb-20 max-w-[510px]">
+                  <span className="font-semibold text-lg text-primary mb-2 block">
+                    Contact US
+                  </span>
+                  <h2 className="font-bold text-3xl sm:text-4xl md:text-[40px] text-dark ">
+                    Leave Message
+                  </h2>
+                </div>
+              </div>
+            </div>
+
+            <form
+              ref={form}
+              onSubmit={sendEmail}
+              className="mx-auto mt-16  sm:mt-20"
+            >
+              <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                <div>
+                  <label
+                    for="first-name"
+                    className="block text-sm font-semibold leading-6 text-gray-900"
+                  >
+                    First name
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      type="text"
+                      name="first-name"
+                      id="first-name"
+                      autocomplete="given-name"
+                      placeholder="Enter Your Name"
+                      className="block w-full  border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label
+                    for="last-name"
+                    className="block text-sm font-semibold leading-6 text-gray-900"
+                  >
+                    Email
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      autocomplete="email"
+                      placeholder="Enter Your Email"
+                      className="block w-full  border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400   sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-2">
+                  <label
+                    for="message"
+                    className="block text-sm font-semibold leading-6 text-gray-900"
+                  >
+                    Message
+                  </label>
+                  <div className="mt-2.5">
+                    <textarea
+                      name="message"
+                      id="message"
+                      rows="4"
+                      placeholder="Enter Your Message"
+                      className="block w-full  border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2  sm:text-sm sm:leading-6"
+                    ></textarea>
+                  </div>
+                </div>
+              </div>
+              <div className="mt-10">
+                <button
+                  type="submit"
+                  className="w-full mb-32 rounded-md bg-primary px-10 py-2.5 text-center text-sm font-semibold text-white   "
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </section>
+    </div>
+  );
+};
 
-      
- 
-
-      </div>
-  )
-}
-
-export default Contact
+export default Contact;
