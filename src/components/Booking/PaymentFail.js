c4f0fdac0a3d90f4e4d940b8b3e597b1157a4f20
Rubayed <rubayedahmed04@gmail.com>
2023-05-06 12:10:02 +0600

    Payment

--- src/components/Booking/PaymentFail.js
+++ src/components/Booking/PaymentFail.js
@@ -1,9 +1,12 @@
 
 const PaymentFail = () => {
     return (
-      <div className="container mx-auto py-32 pl-32 pr-32">
-        Something Went Wrong!
-      </div>
+      <div className="container mx-auto relative my-40 ">
+      <h2
+        className="md:pt-42 md:absolute md:top-[48%] md:left-1/2 md:-translate-x-2/4 md:translate-y-[80%] text-xl md:text-2xl lg:text-3xl font-bold">
+       Something Went Wrong! Try Again
+      </h2>
+    </div>
     );
   };
   
