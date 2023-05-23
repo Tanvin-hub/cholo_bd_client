0c03c8940dfde32abc577b23f4d8d0f818691952
Maruf Hossain <marufrony48@gmail.com>
2023-05-23 23:37:58 +0600

    ee

--- src/components/Trip/TripDetails/TripDetails.js
+++ src/components/Trip/TripDetails/TripDetails.js
@@ -33,36 +33,6 @@ const TripDetails = () => {
       <div className="container mx-auto px-32 py-24">
         <div className="grid grid-cols-3 rounded-lg ">
           <div className="rounded-xl col-start-1 col-end-3 mr-6">
-<<<<<<< HEAD
-            <>
-              <Swiper
-                pagination={{
-                  type: "progressbar",
-                }}
-                navigation={true}
-                modules={[Pagination, Navigation]}
-                className="mySwiper"
-              >
-                {detailsImg?.map((img) => (
-                  <SwiperSlide>
-                    <img
-                      src="https://plus.unsplash.com/premium_photo-1673970474453-7c49815647ee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-                      alt=""
-                      className="h-full w-full"
-                    />
-                  </SwiperSlide>
-                ))}
-              </Swiper>
-            </>
-            <div className="my-6  rounded px-6 py-12 bg-white">
-              <h3 className="text-lg font-bold">Tour Overview</h3>
-              <p>{description}</p>
-            </div>
-            <div className="my-6  rounded px-6 py-12 bg-white">
-              <h3 className="text-lg font-bold">Review</h3>
-              <p>{description}</p>
-            </div>
-=======
           <>
           <Swiper
               pagination={{
@@ -91,7 +61,6 @@ const TripDetails = () => {
                 <h3 className="text-lg font-bold">Review</h3>
                 <p>{description}</p>
           </div>
->>>>>>> b19392ab3c8ffa49c72aa3e109b3db2454251ac6
           </div>
           <div>
             <Booking tour={data} />
